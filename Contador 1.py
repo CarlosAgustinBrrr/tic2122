9599fa65086ac69a6f80baf2ecf13697e9921e9e
CarlosAgustinBrrr <90762647+CarlosAgustinBrrr@users.noreply.github.com>
2021-09-22 11:50:33 +0200

    Add files via upload

---
+++ Contador 1.py
@@ -0,0 +1,8 @@
+def contador_1():
+    print("Voy a contar desde 0 hasta 10:")
+    for cont in range (0,11):
+        print(cont)
+
+contador_1()
+
+    
---
+++ saludador 2.py
@@ -0,0 +1,11 @@
+def saludador2():
+    nombre = raw_input("¿Como te llamas?")
+    print ("Buenos dias," +nombre)
+    edad=raw_input("¿Cuantos anios tienes?")
+    lugar= raw_input("¿Donde vives?")
+    print("tienes" +edad+ " anios")
+    print("y se que vives en" +lugar)
+    print("ten cuidado")                 
+                     
+saludador2()
+
