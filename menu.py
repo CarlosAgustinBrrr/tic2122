d578d7bac9d5aa5aa8fea8476f83dbfbed4ec766
Elchuky <90762647+CarlosAgustinBrrr@users.noreply.github.com>
2021-11-11 08:10:39 +0100

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,36 @@
+def menu():
+    #Pedimos dos numeros
+    while():
+        num1=input("Introduce un numero: ")
+        num2=input("Introduce otro numero: ")
+        error=0
+        print("******************************")
+        print("*           MENUS            *")
+    print("******************************")
+    print("1. SUMAR")
+    print("2. RESTAR")
+    print("3. MULTIPLICACION")
+    print("4. DIVISION")
+    opcion=0
+    while(opcion<=0 or opcion>4):
+        opcion=input(" ELIGE: ")
+        if(num2==0 and opcion==4)
+        error=1
+        pritn("ELECCION ERRONEA, NO PUEDO DIVIDIR POR 0")
+    print("Has elegido la opcion "+str(opcion))
+    #OPCION SUMAR
+    if(opcion==1):
+        print(num1+num2)
+    #OPCION RESTA
+    if(opcion==2):
+        print(num1-num2)
+    #OPCION PRODUCTO
+    if(opcion==3):
+        print(num1*num2)
+    #OPCION COCIENTE
+    if(opcion==4):
+        print(float(num1)/num2)
+            
+menu()
+
+    
