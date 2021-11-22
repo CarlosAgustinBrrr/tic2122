e4ffc89af0974fcfa0fb8be552ddc76bfc976728
Elchuky <90762647+CarlosAgustinBrrr@users.noreply.github.com>
2021-11-22 10:07:55 +0100

    Add files via upload

---
+++ daditos.py
@@ -0,0 +1,26 @@
+import 
+
+
+def dados():
+    """Introduzca el nombre de los jugadores
+    PARTIDA
+    Turno 1:
+    Jugador 1
+    tirada 1= 6
+    Jugador 2
+    tirada 1= 3
+    [...]
+    RESULTADOS
+    Puntos jugador 1=
+    Puntos jugador 2=
+    Ha ganado jugador 2"""
+    print("Introduce el nombre de los jugadores")
+    player1=raw_input("PLAYER ONE: ")
+    player2=raw_input("PLAYER TWO: ")
+    puntos_1=0
+    puntos_2=0
+    while():
+        print("Turno 1")
+        tirada=random.randint()
+        print("Tirada 1 "+player1+" = "+tirada)
+        puntos_1=puntos_1+tirada
